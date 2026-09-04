ba2dd8a8ec0de4fd6a92f3c7ffdd6490401971a9
spath343 <spathri@ucsd.edu>
2026-09-03 18:11:16 -0700

    Create HW3_143b.m

---
+++ MAE143B_SS2/HW_codes/HW3_143b.m
@@ -0,0 +1,10 @@
+clear;close all;clc;
+
+clear; close all, d=0.1; a=1; G=RR_pade(d,2,2)*RR_tf(1,[1 a]); D=16.47; L=G*D; 
+figure(1), RR_rlocus(G*D)
+%%
+omega=16.45;
+figure(2), D=1*real(RR_evaluate(-1/L,1i*omega)), RR_rlocus(G*D)
+%%
+G=RR_pade(d,16,12)*RR_tf(1,[1 a]);D=16.25;
+figure(3); RR_rlocus(G*D)
